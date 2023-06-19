baae2edf07460b0b895d1b71e985e3dba84cc306
Poyraz Avsever <poyrazavsever@hotmail.com>
2023-06-19 19:48:59 +0300

    fix

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from "./Pages/Home/Home";
 import Blog from "./Pages/Writing/Blog"
 import Post from "./Pages/Writing/Post"
 import NotFound from "./Components/NotFound"
-import AnimatedCursor from 'animated-cursor'
+
 
 // Components
 import PreLoader from "./Components/PreLoader"
@@ -13,35 +13,8 @@ import { Route, Routes } from "react-router-dom";
 
 function App() {
   
-  
-
-  let options = {
-    color: '#a3a3a3',
-    outerAlpha: 0.25,
-    size: { 
-      inner: 8, 
-      outer: 38 
-    },
-    hoverScale: {
-      inner: 0.5,
-      outer: 1.4
-    },
-    clickScale: {
-      inner: 1.4,
-      outer: 0.1
-    }
-  }
-
-  const cursor = AnimatedCursor(options)
-  cursor.init()
-
   return (
     <div className="overflow-x-hidden">
-      
-      <div id="cursor" className="!z-50">
-        <div id="cursor-outer"></div>
-        <div id="cursor-inner"></div>
-      </div>
 
       <PreLoader />
 
