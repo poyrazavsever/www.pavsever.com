32c5236b91462aee1fe5e21be9610c2a5d3eade7
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-15 00:44:38 +0300

    feat: added animation for skillcard

--- src/Components/SkillCard.jsx
+++ src/Components/SkillCard.jsx
@@ -1,11 +1,24 @@
 import React from 'react'
 import ReactIcon from "./Icon"
-function SkillCard({link = "", iconName = "", iconType = "", iconColor = "", skillName = ""}) {
+import { motion } from 'framer-motion'
+
+function SkillCard({ link = "", iconName = "", iconType = "", iconColor = "", skillName = "" }) {
   return (
-    <a href={link} target='_blank' without="true" rel="noreferrer" className='inline-flex items-center justify-center gap-2 px-4 py-2 border w-32 md:w-36 border-neutral-800 bg-neutral-950 group hover:border-neutral-600 transition-all'>
-      <ReactIcon iconName={iconName} iconType={iconType} iconColor={iconColor} classname="text-sm md:text-lg"/>
+    <motion.a
+      href={link}
+      target='_blank'
+      without="true"
+      rel="noreferrer"
+      className='inline-flex items-center justify-center gap-2 px-4 py-2 border w-32 md:w-36 border-neutral-800 bg-neutral-950 group transition-all'
+      whileHover={{
+        scale: 1.05,  // Kartı hafifçe büyüt
+        borderColor: 'rgba(160, 160, 160, 0.6)', // Sınır rengini değiştir
+        transition: { duration: 0.4, ease: 'easeInOut' } // Animasyonun süresini ayarla
+      }}
+    >
+      <ReactIcon iconName={iconName} iconType={iconType} iconColor={iconColor} classname="text-sm md:text-lg" />
       <span className='font-medium text-neutral-400 text-sm md:text-base'>{skillName}</span>
-    </a>
+    </motion.a>
   )
 }
 
