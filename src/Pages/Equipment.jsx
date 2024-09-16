b9d70810e4f85081d48736ddf3076ae7f8bc0244
Poyraz Avsever <poyrazavsever@hotmail.com>
2024-09-16 21:42:04 +0300

    feat: added equipments

--- src/Pages/Equipment.jsx
+++ src/Pages/Equipment.jsx
@@ -4,15 +4,51 @@ import { motion } from 'framer-motion';
 const equipmentList = [
   {
     id: 1,
-    name: 'Laptop',
+    name: 'Logitech MK470',
     img: '/equipment/klavye.jpg',
-    description: 'High-performance laptop for development work.'
+    description: 'Logitech MK470 Kablosuz Klavye Mouse Set '
   },
   {
     id: 2,
-    name: 'Monitor',
+    name: 'SoundCore Life Q10',
     img: '/equipment/kulaklık.jpg',
-    description: 'Ultrawide monitor for enhanced productivity.'
+    description: 'Anker SoundCore Life Q10'
+  },
+  {
+    id: 3,
+    name: '32MP58HQ-P',
+    img: '/equipment/monitör2.jpg',
+    description: '32" Class Full HD IPS LED Monitor (31.5" Diagonal)'
+  },
+  {
+    id: 4,
+    name: 'Odyssey G3',
+    img: '/equipment/monitör1.jpg',
+    description: 'Odyssey G3 24” 165 Hz Full HD Çerçevesiz Oyun Monitörü'
+  },
+  {
+    id: 5,
+    name: 'Odyssey G3',
+    img: '/equipment/monitör1.jpg',
+    description: 'Odyssey G3 24” 165 Hz Full HD Çerçevesiz Oyun Monitörü'
+  },
+  {
+    id: 6,
+    name: 'INCA EMPOUSA',
+    img: '/equipment/mousepad.jpg',
+    description: 'INCA EMPOUSA RGB 7 LED MOUSEPAD'
+  },
+  {
+    id: 7,
+    name: 'Logitech Z313 Speaker',
+    img: '/equipment/hoparlör.jpg',
+    description: 'Logitech Z313 980-000413 50W 2+1 Speaker Siyah'
+  },
+  {
+    id: 8,
+    name: 'Soundcore Motion B',
+    img: '/equipment/anker.jpg',
+    description: 'Anker Soundcore Motion B Taşınabilir Bluetooth Hoparlör'
   },
 ];
 
@@ -27,7 +63,7 @@ const cardVariants = {
 
 function Equipment() {
   return (
-    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-12 px-8 md:px-0'>
+    <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 pt-12 px-8 md:px-0 py-12'>
       {equipmentList.map((item) => (
         <motion.div
           key={item.id}
